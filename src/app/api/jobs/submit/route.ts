f50b2e34ef6285ff39d95d0db413fa9937c8a608
DevPBDias <devpbdias@gmail.com>
2024-04-12 16:03:33 -0300

    fix: uso do try/catch para encapsular rota POST /jobs/submit

--- src/app/api/jobs/submit/route.ts
+++ src/app/api/jobs/submit/route.ts
@@ -2,11 +2,17 @@ import { ISubmitData } from "@/types";
 import { NextResponse, NextRequest } from 'next/server'
 
 export async function POST(request: NextRequest) {
-    const { name, age, phone, state, city }: ISubmitData = await request.json();
+    try {
+        const { name, age, phone, state, city }: ISubmitData = await request.json();
 
-    if (!name || !age || !phone || !state || !city) {
-        return NextResponse.json({ message: 'Please submit all fields' }, { status: 400 })
+        if (!name || !age || !phone || !state || !city) {
+            return NextResponse.json({ message: 'Please submit all fields' }, { status: 400 })
+        }
+    
+        return NextResponse.json({ message: `Thank you for your application, ${name}` }, { status: 201 })
+
+    } catch (error: any) {
+        return NextResponse.json({ message: error.message }, { status: 500 })
+        
     }
-
-    return NextResponse.json({ message: `Thank you for your application, ${name}` }, { status: 201 })
 }
