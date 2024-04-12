1c75db08f87be9b3f16e097705e68ed4487b7197
DevPBDias <devpbdias@gmail.com>
2024-04-12 19:56:05 -0300

    styles:  estilização componente navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,26 +3,30 @@ import logo from '../assets/Livia..png'
 import Image from 'next/image'
 import Link from 'next/link'
 import { headerLinks } from '@/utils/navbarLinks'
-
 const Navbar = () => {
     return (
-        <header>
-            <section>
-                <div>
-                    <div>
-                        <Image src={logo} alt="logo icon" />
+        <header className='w-full h-[120px] flex justify-between m-auto items-center'>
+            <section className='w-4/5 flex flex-row justify-between m-auto items-center'>
+                <div className='flex flex-row justify-between gap-x-[188px]'>
+                    <div className='w-[92px] h-[54px] flex items-center'>
+                        <Image className='h-[36px]' src={logo} alt="logo icon" />
                     </div>
-                    <div>
+                    <div className='flex flex-row justify-between m-auto items-center gap-x-[56px]'>
                         {
                             headerLinks?.map((link) => (
-                                <Link key={link.id} href={link.path}>
+                                <Link
+                                    className='font-normal text-lg text-[#FBFBFB]'
+                                    key={link.id}
+                                    href={link.path}>
                                     {link.name}
                                 </Link>
                             ))
                         }
                     </div>
                 </div>
-                <button type="button">
+                <button
+                className='p-2 font-normal text-lg text-[#FBFBFB] bg-[#515DEF]'
+                type="button">
                     Contact Us
                 </button>
             </section>
