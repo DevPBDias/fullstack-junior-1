80874a0e63836d2dc9fe0ac410baf8ea8daaf81a
DevPBDias <devpbdias@gmail.com>
2024-04-12 19:02:42 -0300

    feat: setup do front com fontes, remoção de codigo e adição de imagens

--- src/app/jobs/middleware.ts
+++ src/app/jobs/middleware.ts
@@ -1,6 +1,10 @@
 import { type NextRequest, NextResponse } from 'next/server'
 import { headers } from 'next/headers'
- 
+
+// minha ideia era usar um middleware para checar todas as rotas, mas
+// pelo material que busquei na internet seja pela documentação, artigos
+// ou videos não consegui realizar minha ideia com o secret 'naranja-labs'
+// então coloquei em cada rota mesmo.
 export function middleware(request: NextRequest) {
     const response = NextResponse.next();
     const headersList = headers()
