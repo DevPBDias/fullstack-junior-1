9b86c5390230f3583a9105e4b75e794e1345b1b6
DevPBDias <devpbdias@gmail.com>
2024-04-12 00:59:37 -0300

    feat: criei /jobs?level=Junior para retornar apenas info de jobs leve Junior

--- src/app/jobs/route.ts
+++ src/app/jobs/route.ts
@@ -1,5 +1,15 @@
 import jobsData from "@/database/jobs"
+import { type NextRequest } from 'next/server'
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams;
+    const query = searchParams.get('level');
+
+    const filteredLevel = jobsData.filter((job) => job.level === query)
+
+    if(filteredLevel.length !== 0) {
+        return Response.json(filteredLevel)
+    }
+
     return Response.json(jobsData)
 }
