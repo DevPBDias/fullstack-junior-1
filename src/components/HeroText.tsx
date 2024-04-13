3bcc0f81da3d69bd62e7eab2ab7c5ca5a7612a39
DevPBDias <devpbdias@gmail.com>
2024-04-13 13:23:57 -0300

    styles:  criando classes comuns e reset css

--- src/components/HeroText.tsx
+++ src/components/HeroText.tsx
@@ -4,24 +4,24 @@ import arrowRight from '../../public/assets/icons/arrowRight.svg'
 
 const HeroText = () => {
     return (
-        <section className='mt-[20px] flex flex-col ml-[160px] justify-start'>
-            <p className='font-normal text-2xl text-[#FBFBFB] leading-[40px]'>
+        <section className='mt-[20px] hero_container'>
+            <p className='normal_txt'>
                 Hi, There!
             </p>
-            <h1 className=' font-bold text-[64px] text-[#FBFBFB] leading-[80px]'>
+            <h1 className='bold_title'>
                 We are a growing-up
                 <br />
                 Creative Agency from
                 <br />
                 Indonesia!
             </h1>
-            <p className='mt-[8px] font-normal text-2xl text-[#FBFBFB] leading-10'>
+            <p className='mt-[8px] normal_txt'>
                 We provides you with modest and professional design templates
                 <br />
                 that come from the synergy of design and technology
             </p>
             <button
-            className='mt-[40px] flex flex-row items-center h-[80px] w-[299px] bg-[#FBFBFB] justify-center font-normal text-[24px] text-[#333333] leading-[40px]'
+            className='mt-[40px] h-[80px] w-[299px] main_btn_container main_btn_txt'
             type='button'>
                 Let’s Collaborate
                 <Image className='w-[48px] h-[48px]' src={arrowRight} alt="arrow icon" />
--- src/components/ScrollArrow.tsx
+++ src/components/ScrollArrow.tsx
@@ -4,10 +4,10 @@ import arrowDown from '../../public/assets/icons/arrowDown.png'
 
 const ScrollArrow = () => {
     return (
-        <div className='absolute bottom-[73px] left-[60px] w-[40px] h-[89px] gap-[8px] flex flex-col items-center'>
-            <p className='rotate-[-90deg] text-[#909090] font-normal text-lg -leading-[40px]'>Scroll</p>
+        <div className='bottom-[73px] left-[60px] w-[40px] h-[89px] scroll_arrow_container'>
+            <p className='scroll_arrow_txt'>Scroll</p>
             <div className='w-[32px] h-[32px]'>
-                <Image className='w-full h-full' src={arrowDown} alt="arrow icon" />
+                <Image className='img_size' src={arrowDown} alt="arrow icon" />
             </div>
         </div>
     )
