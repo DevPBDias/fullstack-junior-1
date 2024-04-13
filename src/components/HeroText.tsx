4b154e98cc8d20bd54e4d6514ed9eb185f6af54f
DevPBDias <devpbdias@gmail.com>
2024-04-13 12:19:28 -0300

    styles/fix: alterei as margens do  HeroText

--- src/components/HeroText.tsx
+++ src/components/HeroText.tsx
@@ -8,7 +8,7 @@ const HeroText = () => {
             <p className='font-normal text-2xl text-[#FBFBFB] leading-[40px]'>
                 Hi, There!
             </p>
-            <h1 className='mt-[4px] font-bold text-[64px] text-[#FBFBFB] leading-[80px]'>
+            <h1 className=' font-bold text-[64px] text-[#FBFBFB] leading-[80px]'>
                 We are a growing-up
                 <br />
                 Creative Agency from
@@ -21,7 +21,7 @@ const HeroText = () => {
                 that come from the synergy of design and technology
             </p>
             <button
-            className='mt-[36px] flex flex-row items-center h-[80px] w-[299px] bg-[#FBFBFB] justify-center font-normal text-[24px] text-[#333333] leading-[40px]'
+            className='mt-[40px] flex flex-row items-center h-[80px] w-[299px] bg-[#FBFBFB] justify-center font-normal text-[24px] text-[#333333] leading-[40px]'
             type='button'>
                 Let’s Collaborate
                 <Image className='w-[48px] h-[48px]' src={arrow} alt="arrow icon" />
